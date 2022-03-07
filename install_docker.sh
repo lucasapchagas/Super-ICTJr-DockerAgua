f85b3bfe68c02ebb7b0864b6808134ea3f0437b9
Lucas Chagas <lucas.chagas@icomp.ufam.edu.br>
2022-03-07 01:26:28 +0000

    Script de instalação docker
    Signed-off-by: Lucas Chagas <lucas.chagas@icomp.ufam.edu.br>

---
+++ install_docker.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+wget https://download.docker.com/linux/ubuntu/dists/focal/pool/stable/amd64/docker-ce-cli_20.10.9~3-0~ubuntu-focal_amd64.deb
+wget https://download.docker.com/linux/ubuntu/dists/focal/pool/stable/amd64/docker-ce_20.10.9~3-0~ubuntu-focal_amd64.deb
+wget https://download.docker.com/linux/ubuntu/dists/focal/pool/stable/amd64/containerd.io_1.4.9-1_amd64.deb
+
+sudo dpkg -i docker-ce-cli_20.10.9~3-0~ubuntu-focal_amd64.deb
+sudo dpkg -i containerd.io_1.4.9-1_amd64.deb
+sudo dpkg -i docker-ce_20.10.9~3-0~ubuntu-focal_amd64.deb
+
+rm docker-ce-cli_20.10.9~3-0~ubuntu-focal_amd64.deb
+rm containerd.io_1.4.9-1_amd64.deb
+rm docker-ce_20.10.9~3-0~ubuntu-focal_amd64.deb
