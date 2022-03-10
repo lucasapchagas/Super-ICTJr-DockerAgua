84fe87dbf714ac4cff73107489079078ac4c4f47
Lucas Chagas <91504197+lucasapchagas@users.noreply.github.com>
2022-03-10 00:02:53 -0400

    Update install_docker.sh

--- install_docker.sh
+++ install_docker.sh
@@ -11,3 +11,9 @@ sudo dpkg -i docker-ce_20.10.9~3-0~ubuntu-focal_amd64.deb
 rm docker-ce-cli_20.10.9~3-0~ubuntu-focal_amd64.deb
 rm containerd.io_1.4.9-1_amd64.deb
 rm docker-ce_20.10.9~3-0~ubuntu-focal_amd64.deb
+
+sudo docker pull synomi/thingspeak
+sudo docker run --name mysql55 -e MYSQL_ROOT_PASSWORD=speak -d mysql:5.5
+sudo docker run --name thingspeak -p 80:80 --link mysql55:mysql -d synomi/thingspeak
+sudo docker exec -it thingspeak rake db:create
+sudo docker exec -it thingspeak rake db:schema:load
