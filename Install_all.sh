432d2a20b7164c54003b26f44137e0de174155d0
Lucas Chagas <91504197+lucasapchagas@users.noreply.github.com>
2022-03-10 20:56:58 -0400

    Update Install_all.sh

--- Install_all.sh
+++ Install_all.sh
@@ -4,13 +4,15 @@ wget https://download.docker.com/linux/ubuntu/dists/focal/pool/stable/amd64/dock
 wget https://download.docker.com/linux/ubuntu/dists/focal/pool/stable/amd64/docker-ce_20.10.9~3-0~ubuntu-focal_amd64.deb
 wget https://download.docker.com/linux/ubuntu/dists/focal/pool/stable/amd64/containerd.io_1.4.9-1_amd64.deb
 
-sudo dpkg -i containerd.io_1.4.9-1_amd64.deb
 sudo dpkg -i docker-ce-cli_20.10.9~3-0~ubuntu-focal_amd64.deb
+sudo dpkg -i containerd.io_1.4.9-1_amd64.deb
 sudo dpkg -i docker-ce_20.10.9~3-0~ubuntu-focal_amd64.deb
 
 rm docker-ce-cli_20.10.9~3-0~ubuntu-focal_amd64.deb
 rm containerd.io_1.4.9-1_amd64.deb
 rm docker-ce_20.10.9~3-0~ubuntu-focal_amd64.deb
+
+sudo docker run hello-world
 
 sudo docker pull synomi/thingspeak
 sudo docker run --name mysql55 -e MYSQL_ROOT_PASSWORD=speak -d mysql:5.5
