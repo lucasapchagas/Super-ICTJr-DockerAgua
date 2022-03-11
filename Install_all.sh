aee8f6784b67790d7a4b35b171f7802107b73e01
Lucas Chagas <91504197+lucasapchagas@users.noreply.github.com>
2022-03-10 20:29:43 -0400

    Update Install_all.sh

--- Install_all.sh
+++ Install_all.sh
@@ -4,8 +4,8 @@ wget https://download.docker.com/linux/ubuntu/dists/focal/pool/stable/amd64/dock
 wget https://download.docker.com/linux/ubuntu/dists/focal/pool/stable/amd64/docker-ce_20.10.9~3-0~ubuntu-focal_amd64.deb
 wget https://download.docker.com/linux/ubuntu/dists/focal/pool/stable/amd64/containerd.io_1.4.9-1_amd64.deb
 
-sudo dpkg -i docker-ce-cli_20.10.9~3-0~ubuntu-focal_amd64.deb
 sudo dpkg -i containerd.io_1.4.9-1_amd64.deb
+sudo dpkg -i docker-ce-cli_20.10.9~3-0~ubuntu-focal_amd64.deb
 sudo dpkg -i docker-ce_20.10.9~3-0~ubuntu-focal_amd64.deb
 
 rm docker-ce-cli_20.10.9~3-0~ubuntu-focal_amd64.deb
